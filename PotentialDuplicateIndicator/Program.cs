1a0b3ac71d119b23a200d99d9824548616ce6904
ROBBY <dpkimchi@gmail.com>
2021-03-30 20:36:24 -0700

    Add project files.

---
+++ PotentialDuplicateIndicator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace PotentialDuplicateIndicator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //C:\Users\chilc\Desktop\advertisers.txt
+
+            #region Initials
+            string fileLocation = "";
+            bool isPathValid = false;
+            bool isFileValid = false;
+            #endregion
+
+            #region Process
+            Console.WriteLine("Welcome to Potential Duplicate Indicator !!\n");
+            Console.WriteLine("\nPlease input file location: ");
+            fileLocation = Console.ReadLine();
+
+
+            if (fileLocation == "")
+            {
+                Console.WriteLine("Cannot find the file, please try again !");
+            }
+            else
+            {
+                isPathValid = true;
+            }
+
+            while (isPathValid)
+            {
+                try
+                {
+                    string[] name_array = File.ReadAllLines(fileLocation);
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("The input file location is invalid !");
+                }
+
+                Console.WriteLine("Welcome to Potential Duplicate Indicator !!\n");
+                Console.WriteLine("\nPlease input file location: ");
+                fileLocation = Console.ReadLine();
+            }
+            #endregion
+
+        }
+
+        #region Method
+        public void ReadFile()
+        {
+
+        }
+
+        public void Compare()
+        {
+
+        }
+        #endregion
+    }
+}
